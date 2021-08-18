9344c917436ed3c8899d7b9c1b22e59a34922afd
Amber <amberleeBME@outlook.com>
2021-08-17 22:38:59 -0400

    clean up

--- EmployeeSQL/employee_schema.sql
+++ EmployeeSQL/employee_schema.sql
@@ -1,75 +1,47 @@
-﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
-
-CREATE TABLE "titles" (
-    "title_id" VARCHAR(30)   NOT NULL,
-    "title" VARCHAR(30)   NOT NULL,
-    CONSTRAINT "pk_titles" PRIMARY KEY (
-        "title_id"
-     )
+﻿CREATE TABLE titles (
+    title_id VARCHAR(30)   NOT NULL,
+    title VARCHAR(30)   NOT NULL,
+    PRIMARY KEY (title_id)
 );
 
-CREATE TABLE "employees" (
-    "emp_no" INT   NOT NULL,
-    "emp_title_id" VARCHAR(30)   NOT NULL,
-    "birth_date" DATE   NOT NULL,
-    "first_name" VARCHAR(30)   NOT NULL,
-    "last_name" VARCHAR(30)   NOT NULL,
-    "sex" VARCHAR(30)   NOT NULL,
-    "hire_date" DATE   NOT NULL,
-    CONSTRAINT "pk_employees" PRIMARY KEY (
-        "emp_no"
-     )
+CREATE TABLE employees (
+    emp_no INT   NOT NULL,
+    emp_title_id VARCHAR(30)   NOT NULL,
+    birth_date DATE   NOT NULL,
+    first_name VARCHAR(30)   NOT NULL,
+    last_name VARCHAR(30)   NOT NULL,
+    sex VARCHAR(30)   NOT NULL,
+    hire_date DATE   NOT NULL,
+    FOREIGN KEY (emp_title_id) REFERENCES titles(title_id),
+    PRIMARY KEY (emp_no)
 );
 
-CREATE TABLE "departments" (
-    "dept_no" VARCHAR(30)   NOT NULL,
-    "dept_name" VARCHAR(30)   NOT NULL,
-    CONSTRAINT "pk_departments" PRIMARY KEY (
-        "dept_no"
-     )
+CREATE TABLE departments (
+    dept_no VARCHAR(30)   NOT NULL,
+    dept_name VARCHAR(30)   NOT NULL,
+    PRIMARY KEY (dept_no)
 );
 
-CREATE TABLE "dept_emp" (
-    "de_id" INT   NOT NULL,
-    "emp_no" INT   NOT NULL,
-    "dept_no" VARCHAR(30)   NOT NULL,
-    CONSTRAINT "pk_dept_emp" PRIMARY KEY (
-        "de_id"
-     )
+CREATE TABLE salaries (
+    emp_no INT   NOT NULL,
+    salary INT   NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+    PRIMARY KEY (emp_no, salary)
 );
 
-CREATE TABLE "dept_manager" (
-    "dm_id" INT   NOT NULL,
-    "dept_no" VARCHAR(30)   NOT NULL,
-    "emp_no" INT   NOT NULL,
-    CONSTRAINT "pk_dept_manager" PRIMARY KEY (
-        "dm_id"
-     )
+-- JUNCTION TABLES
+CREATE TABLE dept_emp (
+    emp_no INT   NOT NULL,
+    dept_no VARCHAR(30)   NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+    PRIMARY KEY (emp_no, dept_no)
 );
 
-CREATE TABLE "salaries" (
-    "salaries_id" INT   NOT NULL,
-    "emp_no" INT   NOT NULL,
-    "salary" INT   NOT NULL,
-    CONSTRAINT "pk_salaries" PRIMARY KEY (
-        "salaries_id"
-     )
+CREATE TABLE dept_manager (
+    dept_no VARCHAR(30)   NOT NULL,
+    emp_no INT   NOT NULL,
+    FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+    PRIMARY KEY (dept_no, emp_no)
 );
-
-ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY("emp_title_id")
-REFERENCES "titles" ("title_id");
-
-ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
-
-ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "departments" ("dept_no");
-
-ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "departments" ("dept_no");
-
-ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
-
-ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
